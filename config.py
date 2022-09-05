af2b5ec41271a95ba8b523d1fedba701edcad360
KGBUSH <dykgbush@gmail.com>
2022-09-05 22:42:26 +0800

    [init]

---
+++ config.py
@@ -0,0 +1,15 @@
+dis_learning_rate = 0.0001
+gen_learning_rate = 0.001
+num_epochs = 200
+batch_size = 64
+train_size = 0.5
+
+num_data = 60
+
+gen_a_save_path = './models/gen_a.pt'
+gen_b_save_path = './models/gen_b.pt'
+building_name = 'CP1'
+test_building_name = 'OIE'
+
+temperature_min = 0
+temperature_max = 40
